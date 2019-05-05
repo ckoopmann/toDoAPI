176ff34484c4ae479d890228ef968de5b1dc468b
Christian Koopmann <c.k.e.koopmann@gmail.com>
2019-05-05 15:30:04 +0200

    Additional unit tests for getting and deleting todos in the database

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -12,3 +12,12 @@ def test_can_get_todo(db_session, todo):
     db_session.commit()
     obj = db_session.query(Todo).filter(Todo.id == todo.id).first()
     assert obj.id == todo.id
+
+def test_can_delete_todo(db_session, todo):
+    db_session.add(todo)
+    db_session.commit()
+    num_deleted = db_session.query(Todo).filter(Todo.id == todo.id).delete()
+    assert num_deleted == 1
+    db_session.commit()
+    num_left = db_session.query(Todo).filter(Todo.id == todo.id).count()
+    assert num_left == 0
